813353b3b48c76eb5700b64cb81132ebc7d98022
Adam Tait <adam@adamtait.com>
2011-07-05 00:25:19 -0700

    refactored CreateRequest method, added css

--- js/expanz.js
+++ js/expanz.js
@@ -2,8 +2,32 @@
 // SDK Functions
 //
 
+// WebService URL
+// NOTE: this needs to be better abstracted (also, not currently applied because of cross domain request limitations on browser)
+var wsURL = "https://test.expanz.com/ESADemoService/ESAService.asmx"
 
-function MakeRequest ( xmlrequest ){
+var soapHeader = '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:s="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><SOAP-ENV:Body>';
+
+var soapFooter = '</SOAP-ENV:Body></SOAP-ENV:Envelope>';
+
+
+function CreateRequest( type, username, password ){
+
+	var body = "";
+
+	if( type == 'CreateSession' ){
+
+		body = '<tns:CreateSession xmlns:tns="http://tempuri.org/"><tns:inXml>&lt;ESA&gt; &lt;CreateSession user="demo" password="demo" appSite="ESADEMO" authenticationMode="Primary" clientVersion="Flex 1.0" schemaVersion="2.0"/&gt; &lt;/ESA&gt;</tns:inXml></tns:CreateSession>';
+
+	}
+
+	return soapHeader + body + soapFooter;
+
+}
+
+
+
+function SendRequest ( xmlrequest ){
 
 	$.ajax({
 		type: "post",
