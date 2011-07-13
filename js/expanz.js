497826423a90651bcd70f2d64bd9ac0e35944a1f
Adam Tait <adam@adamtait.com>
2011-07-13 15:34:53 -0700

    completed valid CreateSession request. Refactoring to separate concerns between SDK and client code (more to come)

--- js/expanz.js
+++ js/expanz.js
@@ -4,20 +4,34 @@
 
 // WebService URL
 // NOTE: this needs to be better abstracted (also, not currently applied because of cross domain request limitations on browser)
-var wsURL = "https://test.expanz.com/ESADemoService/ESAService.asmx"
+var wsURL = 'https://test.expanz.com/ESADemoService/ESAService.asmx';
 
-var soapHeader = '<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:s="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><SOAP-ENV:Body>';
+var soapHeader = 	'<SOAP-ENV:Envelope ' +
+				'xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" ' +
+				'xmlns:s="http://www.w3.org/2001/XMLSchema" ' +
+				'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' +
+			'>' +
+			'<SOAP-ENV:Body>';
 
-var soapFooter = '</SOAP-ENV:Body></SOAP-ENV:Envelope>';
+var soapFooter = 	'</SOAP-ENV:Body>' +
+			'</SOAP-ENV:Envelope>';
 
 
 function CreateRequest( type, username, password ){
 
-	var body = "";
+	var body = '';
 
 	if( type == 'CreateSession' ){
 
-		body = '<tns:CreateSession xmlns:tns="http://tempuri.org/"><tns:inXml>&lt;ESA&gt; &lt;CreateSession user="demo" password="demo" appSite="ESADEMO" authenticationMode="Primary" clientVersion="Flex 1.0" schemaVersion="2.0"/&gt; &lt;/ESA&gt;</tns:inXml></tns:CreateSession>';
+		body = 	'<tns:CreateSession xmlns:tns="http://tempuri.org/">' +
+			'<tns:inXml>' +
+				'&lt;ESA&gt; &lt;CreateSession user="' + username + '" ' +
+				'password="' + password + '" ' +
+				'appSite="ESADEMO" ' +
+				'authenticationMode="Primary" ' +
+				'clientVersion="Flex 1.0" ' +
+				'schemaVersion="2.0"/&gt; &lt;/ESA&gt;' +
+			'</tns:inXml></tns:CreateSession>';
 
 	}
 
@@ -25,9 +39,42 @@ function CreateRequest( type, username, password ){
 
 }
 
+function CreateRequestSimple( type, username, password ){
 
+	var body = '';
 
-function SendRequest ( xmlrequest ){
+	if( type == 'CreateSession' ){
+
+		body = 	'<ESA xmlns="http://ns.expanz.com/ep/2008">' +
+			'<CreateSession ' +
+				'user="' + username + '" ' +
+				'password="' + password + '" ' +
+				'appSite="ESA.BANKING" ' +
+				'authDomain="Mattdone-PC" ' +
+				'station="MATTDONE-PC" ' +
+				'clientType="Win32" ' +
+				'clientVersion="2.1" ' +
+				'schemaVersion="2.0" ' +
+				'authenticationMode="Primary" ' +
+				'TimeZone=""/>' +
+				'</ESA>';
+	}
+
+	return soapHeader + body + soapFooter;
+}
+
+function parseXML( xml ){
+
+	$(xml).find( 'CreateSessionResponse' ).each( function ()
+	{
+		//$(this).each( $('#result').append( $(this).text ) );
+		$('#result').append( '<div>' + $(this).find('CreateSessionResult').text() + '</div>' );
+		$('#result').append( $(this).find('errorMessage').text() );
+
+	});
+}
+
+function SendRequest ( xmlrequest, callback ){
 
 	$.ajax({
 		type: "post",
@@ -36,11 +83,7 @@ function SendRequest ( xmlrequest ){
 		contentType: "text/xml",
 		dataType: "xml",
 		processData: false,	//keeps data: from being serialized
-		success:
-			function (message) {
-				// do something with /message/
-				$('#result').append( message );
-			}
+		success: parseXML
 	});
 }
 
