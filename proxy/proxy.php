cca348805218eed4bbe1b646cd60fae1d8c3ee65
Adam Tait <adam@adamtait.com>
2011-09-19 12:54:11 -0400

    remove debug output

--- proxy/proxy.php
+++ proxy/proxy.php
@@ -20,17 +20,6 @@ list( $header, $contents ) = preg_split( '/([\r\n][\r\n])\\1/', curl_exec( $ch )
 $status = curl_getinfo( $ch );
 curl_close( $ch );
 
-$filename = './log';
-$f = fopen( $filename, 'w+' );
-fwrite( $f, 'U: ' . $url . "\n" );
-fwrite( $f, 'D: ' . $data . "\n" );
-fwrite( $f, 'H: ' . $header . "\n" );
-fwrite( $f, 'C: ' . $contents . "\n" );
-$postContents = print_r($_POST, TRUE);
-fwrite( $f, 'All: ' . "$postContents" . "\n" );
-fwrite( $f, 'Raw: ' . $HTTP_RAW_POST_DATA . "\n" );
-fclose( $f );
-  
 print $header;
 
 ?>
