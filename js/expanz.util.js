2b34b37209dfb80cb76268db464cc37bfd60f49a
Kim Damevin <kdamevin@gmail.com>
2012-01-12 16:47:31 +1100

    new util class

---
+++ js/expanz.util.js
@@ -0,0 +1,55 @@
+function pop(ary, map) { // , name, key ) {
+
+	for ( var i = 0; i < ary.length; i++) {
+		if (_.reduce(map, function(memo, key) {
+			return memo && (map.key === ary[i].key);
+		}, true)) {
+			var found = ary[i];
+			ary.remove(i);
+			return found;
+		}
+	}
+	return null;
+};
+
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+	var rest = this.slice((to || from) + 1 || this.length);
+	this.length = from < 0 ? this.length + from : from;
+	return this.push.apply(this, rest);
+};
+
+Array.prototype.clone = function() {
+	return JSON.parse(JSON.stringify(this));
+}
+
+XMLDocumentsToXMLString = function(xmlDoc) {
+	if (window.ActiveXObject) {
+		var str = xmlDoc.xml;
+		return str;
+	}
+	// code for Mozilla, Firefox, Opera, etc.
+	else {
+		var str = (new XMLSerializer()).serializeToString(xmlDoc);
+		return str;
+	}
+}
+
+boolValue = function(val) {
+	if (val == null || val == undefined || val.length == 0)
+		return false;
+	val = val.toUpperCase();
+	if (val == "1" || val == "TRUE" || val == "Y" || val == "YES" || val == "ON" || val == "1.00" || val == "ENABLED")
+		return true;
+	return false;
+}
+boolString = function(val) {
+	if (val)
+		return "1";
+	else
+		return "0";
+}
+
+function isNumber(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
