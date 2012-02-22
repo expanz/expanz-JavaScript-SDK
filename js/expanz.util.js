9ac1cf2e8085fd61df9fd088af965e96c7b38a45
Kim Damevin <kdamevin@gmail.com>
2012-02-22 13:15:21 +1100

    new util method: loadjscssfile

--- js/expanz.util.js
+++ js/expanz.util.js
@@ -91,6 +91,22 @@ function getQueryParameterByName(name)
     return decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+function loadjscssfile(filename, filetype){
+	 if (filetype=="js"){ //if filename is a external JavaScript file
+	  var fileref=document.createElement('script')
+	  fileref.setAttribute("type","text/javascript")
+	  fileref.setAttribute("src", filename)
+	 }
+	 else if (filetype=="css"){ //if filename is an external CSS file
+	  var fileref=document.createElement("link")
+	  fileref.setAttribute("rel", "stylesheet")
+	  fileref.setAttribute("type", "text/css")
+	  fileref.setAttribute("href", filename)
+	 }
+	 if (typeof fileref!="undefined")
+	  document.getElementsByTagName("head")[0].appendChild(fileref)
+	}
+
 
 /**
  * Center a element with jQuery
