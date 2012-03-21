4afd6297aa4f2ce80384549e61532a32f6316f93
kdamevin <kdamevin@gmail.com>
2012-03-21 11:57:37 +1100

    jslint

--- js/expanz.util.js
+++ js/expanz.util.js
@@ -64,7 +64,7 @@ Array.prototype.remove = function(from, to) {
 	var rest = this.slice((to || from) + 1 || this.length);
 	this.length = from < 0 ? this.length + from : from;
 	return this.push.apply(this, rest);
-}
+};
 
 Array.prototype.clone = function() {
 	return JSON.parse(JSON.stringify(this));
