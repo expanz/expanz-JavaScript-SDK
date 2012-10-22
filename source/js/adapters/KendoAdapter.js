35e6ce0121b0c7d0c5cd2afb54ae0adfbfd65fde
Stephen neander <stephen.neander@erp365.net>
2012-10-22 11:07:33 +1100

    Allow for multiple combos

--- source/js/adapters/KendoAdapter.js
+++ source/js/adapters/KendoAdapter.js
@@ -27,22 +27,31 @@ function renderKendoComponents(el) {
 
 	/* rendering combo boxes and dropdown */
 	if (el.find("[renderingType=combobox]").length > 0)
-		el.find("[renderingType=combobox]").KendoComboBoxAdapter();
+		el.find("[renderingType=combobox]").each(function() {
+			$(this).KendoComboBoxAdapter();
+		});
 
-	if (el.find("[renderingType=dropdownlist]").length > 0)
-		el.find("[renderingType=dropdownlist]").KendoDropDownListAdapter();
+	if (el.find("[renderingType=dropdownlist]").length > 0) {
+		el.find("[renderingType=dropdownlist]").each(function() {
+			$(this).KendoDropDownListAdapter();
+		});
+	}
 
 	/* bind menus to kendo ui menu */
 	if (el.find('[bind=menu] > ul.menu').length > 0)
-		el.find('[bind=menu] > ul.menu').kendoMenu();
+		el.find('[bind=menu] > ul.menu').each(function() {
+			$(this).kendoMenu();
+		});
 
 	/* bind numeric stepper */
 	if (el.find("[renderingType=numerictextbox]").length > 0)
-		el.find("[renderingType=numerictextbox]").kendoNumericTextBox({
-			min : 1,
-			max : 99,
-			step : 1,
-			format : "n0"
+		el.find("[renderingType=numerictextbox]").each(function() {
+			$(this).kendoNumericTextBox({
+				min : 1,
+				max : 99,
+				step : 1,
+				format : "n0"
+			});
 		});
 }
 
