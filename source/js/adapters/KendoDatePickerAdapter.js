0c2b1da753b8aa8d6e87a2ae67ce634edbf927a0
Chris Anderson <christhecoder@gmail.com>
2012-11-13 15:25:58 +1100

    Created kendo date and time picker adapters.

---
+++ source/js/adapters/KendoDatePickerAdapter.js
@@ -0,0 +1,8 @@
+$.fn.KendoDatePickerAdapter = function() {
+
+	/* instantiate Kendo Control */
+    var control = $(this).kendoDatePicker({
+        // display month and year in the input
+        format: "dd/MM/yyyy"
+	});
+};
---
+++ source/js/adapters/KendoTimePickerAdapter.js
@@ -0,0 +1,12 @@
+$.fn.KendoTimePickerAdapter = function() {
+
+    /* instantiate Kendo Control */
+    var timeFormat = this.attr('timeFormat') !== undefined ? this.attr('timeFormat') : window.config._timeFormat;
+    
+    if (timeFormat === undefined)
+        timeFormat = 12;
+    
+    var control = $(this).kendoTimePicker({
+        format: timeFormat == 12 ? "h:mm tt" : "H:mm"
+    });
+};
