fb4593e9f5dd37a55bc1809a279cd54887fd9043
Chris Anderson <christhecoder@gmail.com>
2012-11-30 16:36:35 +1100

    Now supports having a sub message view.

--- source/js/expanz/views/expanz.views.LoginView.js
+++ source/js/expanz/views/expanz.views.LoginView.js
@@ -18,6 +18,25 @@ $(function () {
 
         initialize: function () {
             this.model.bind("change:isLoggingIn", this.isLoggingInChanged(), this);
+
+            // Look for a message control within the view
+            this.messageControl = $(this.el).find('[bind=messageControl]');
+            
+            if (this.messageControl.length === 0) {
+                // Message control not found within the view - look in the page
+                this.messageControl = $('[bind=messageControl]');
+            }
+
+            if (this.messageControl.length !== 0) {
+                var $messageControl = $(this.messageControl);
+                
+                var view = new expanz.views.MessagesView({
+                    id: $messageControl.attr('id'),
+                    collection: this.model.messageCollection
+                });
+
+                $messageControl.html(view.render().el);
+            }
         },
 
         events: {
@@ -25,14 +44,16 @@ $(function () {
         },
 
         attemptLogin: function () {
+            this.model.messageCollection.reset();
+            
             var usernameEl = this.el.find("#username input");
             var passwordEl = this.el.find("#password input");
             
             if (usernameEl.length === 0 || passwordEl.length === 0) {
-                expanz.messageController.addErrorMessageByText("username or password field cannot be found on the page");
+                alert("Username or password field cannot be found on the page");
             }
             else if (usernameEl.val().length === 0 || passwordEl.val().length === 0) {
-                expanz.messageController.addErrorMessageByKey("loginOrPasswordEmpty");
+                this.model.messageCollection.addErrorMessageByKey("loginOrPasswordEmpty");
             }
             else {
                 this.model.login(usernameEl.val(), passwordEl.val(), this.el.attr('type') == 'popup');
