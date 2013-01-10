ee1d6b2b48796cc766c4d4e0e79e840397e83f9b
Chris Anderson <christhecoder@gmail.com>
2013-01-10 18:21:02 +1100

    Removed core.js, as it doesn't belong in the SDK, and is no longer used anyhow.

--- build/expanz-components.js
+++ build/expanz-components.js
@@ -1,91 +1,4 @@
-﻿///#source 1 1 /source/js/components/core.js
-/* Author: kdamevin
-
- */
-$(function() {
-
-	window.expanz = window.expanz || {};
-
-	window.expanz.Core = window.expanz.Component
-		.extend({
-
-			componentName : 'core',
-
-			modules : window.expanz.Component.prototype.modules.concat([
-				'Header',
-				//'MainMenu',
-				//'Notification',
-				'Footer'
-			]),
-
-			initialize : function() {
-				var that = this;
-				//window.expanz.Component.prototype.componentName = this.componentName;
-				window.expanz.Component.prototype.initialize.call(this);
-				/* render component if present in the page */
-				/*_.each(this.modules, function(module) {
-					var moduleEl = window.expanz.html.findComponentModuleElement(that.componentName, module);
-					if (moduleEl !== undefined) {
-						moduleEl.each(function() {
-							// if (moduleEl.attr('loaded') === undefined) {
-							var moduleContent = that['render' + module + 'Module']($(this));
-							$(this).append(moduleContent);
-							moduleEl.attr('loaded', '1');
-							// }
-						});
-
-					}
-				});*/
-				
-				this._executeAfterInitalize(that);
-			},
-			
-			/*_executeAfterInitalize : function(that) {
-				_.each(this.modules, function(module) {
-					var componentEl = window.expanz.html.findComponentModuleElement(this.componentName, module);
-					if (componentEl !== undefined) {
-						componentEl.each(function() {*/
-							/* execute script after adding to content to the dom */
-							/*if (that['_executeAfterRender' + module + 'Module']) {
-								that['_executeAfterRender' + module + 'Module']($(this));
-							}
-						});
-
-					}
-				});
-			},*/
-
-			renderHeaderModule : function(el) {
-				var siteName = el.attr('siteName') !== undefined ? el.attr('siteName') : window.config._siteName;
-				var siteUrl = el.attr('siteUrl') !== undefined ? el.attr('siteUrl') : getSiteUrl();
-				var html = '';
-				html += window.expanz.html.renderHeader(siteName, siteUrl);
-				return html;
-			},
-			
-			/*renderMainMenuModule : function(el) {
-				var html = '';
-				html += window.expanz.html.renderMainMenu();
-				return html;
-			},
-			
-			renderNotificationModule : function(el) {
-				var html = '';
-				html += window.expanz.html.renderNotification();
-				return html;
-			},*/
-			
-			renderFooterModule : function(el) {
-				var html = '';
-				html += window.expanz.html.renderFooter();
-				return html;
-			}
-
-		});
-	
-});
-
-///#source 1 1 /source/js/components/shoppingCart.js
+﻿///#source 1 1 /source/js/components/shoppingCart.js
 /* Author: kdamevin
 
  */
@@ -1389,7 +1302,7 @@ $(function() {
 
 	window.expanz.html.anonymousPersonActivity = "ERP.Person";
 
-	window.expanz.html.submitLostPasswordForm = function(loginCode, emailAddress) {
+	window.expanz.html.submitLostPasswordForm = function(loginCode, emailAddress, callbacks) {
 	    var xml = '<Activity id="' + window.expanz.html.anonymousPersonActivity + '">';
 	    
 		if (loginCode) {
@@ -1404,7 +1317,7 @@ $(function() {
 		xml += '</Method>';
 		xml += '</Activity>';
 	    
-		window.expanz.net.CreateAnonymousRequest(xml);
+		window.expanz.net.CreateAnonymousRequest(xml, callbacks);
 	};
 
 	window.expanz.html.renderNumericTextBoxesOnTableRenderedEvent = function(hostEl, initValue, min, max) {
