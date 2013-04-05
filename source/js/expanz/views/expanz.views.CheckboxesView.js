24dbfebd6fc95439c78f73e901df2ffe4ca83ac7
Chris Anderson <christhecoder@gmail.com>
2013-04-05 15:34:46 +1100

    Commented out the CheckboxesView and RadioButtonsView code, as it is no longer valid with the removal of the GridView, and need to be rewritten if they are to be used again.

--- source/js/expanz/views/expanz.views.CheckboxesView.js
+++ source/js/expanz/views/expanz.views.CheckboxesView.js
@@ -9,42 +9,42 @@
 //  in accordance with the terms of the license agreement accompanying it.
 //
 ////////////////////////////////////////////////////////////////////////////////
-$(function () {
+//$(function () {
 
-    window.expanz = window.expanz || {};
-    window.expanz.views = window.expanz.views || {};
+//    window.expanz = window.expanz || {};
+//    window.expanz.views = window.expanz.views || {};
 
-    window.expanz.views.CheckboxesView = expanz.views.DataControlView.extend({
-        publishData: function () {
-            /* clean elements */
-            this.$el.html();
-            var that = this;
-            /* no external component needed just have to draw the checkboxes and handle the clicks */
+//    window.expanz.views.CheckboxesView = expanz.views.DataControlView.extend({
+//        publishData: function () {
+//            /* clean elements */
+//            this.$el.html();
+//            var that = this;
+//            /* no external component needed just have to draw the checkboxes and handle the clicks */
 
-            _.each(this.model.get('xml').find('Row'), function (row) {
-                var rowId = $(row).attr('id');
-                var selected = boolValue($(row).attr('selected')) === true ? ' checked="checked" ' : '';
-                _.each($(row).find('Cell'), function (cell) {
-                    var text = $(cell).text();
-                    var id = that.model.id.replace(/\./g, "_") + "_" + rowId;
-                    that.$el.append("<div><input " + selected + " id='" + id + "' value='" + rowId + "' name='checkbox' type='checkbox'></input><span>" + text + "</span></div>");
+//            _.each(this.model.get('xml').find('Row'), function (row) {
+//                var rowId = $(row).attr('id');
+//                var selected = boolValue($(row).attr('selected')) === true ? ' checked="checked" ' : '';
+//                _.each($(row).find('Cell'), function (cell) {
+//                    var text = $(cell).text();
+//                    var id = that.model.id.replace(/\./g, "_") + "_" + rowId;
+//                    that.$el.append("<div><input " + selected + " id='" + id + "' value='" + rowId + "' name='checkbox' type='checkbox'></input><span>" + text + "</span></div>");
 
-                    /* handle checkboxes click */
-                    that.$el.find("#" + id).click(function () {
-                        // window.expanz.logToConsole(that.model.id + " filtered with " + $(this).val());
-                        /* send negative value of id to say it has been unselected */
-                        var val = $(this).val();
-                        if (!$(this).is(":checked")) {
-                            val = -val;
-                        }
-                        /* send the delta to the server */
-                        that.model.updateItemSelected(val);
-                    });
+//                    /* handle checkboxes click */
+//                    that.$el.find("#" + id).click(function () {
+//                        // window.expanz.logToConsole(that.model.id + " filtered with " + $(this).val());
+//                        /* send negative value of id to say it has been unselected */
+//                        var val = $(this).val();
+//                        if (!$(this).is(":checked")) {
+//                            val = -val;
+//                        }
+//                        /* send the delta to the server */
+//                        that.model.updateItemSelected(val);
+//                    });
 
-                });
-            });
+//                });
+//            });
 
-        }
+//        }
 
-    });
-});
+//    });
+//});
--- source/js/expanz/views/expanz.views.RadioButtonsView.js
+++ source/js/expanz/views/expanz.views.RadioButtonsView.js
@@ -9,36 +9,36 @@
 //  in accordance with the terms of the license agreement accompanying it.
 //
 ////////////////////////////////////////////////////////////////////////////////
-$(function () {
+//$(function () {
 
-    window.expanz = window.expanz || {};
-    window.expanz.views = window.expanz.views || {};
+//    window.expanz = window.expanz || {};
+//    window.expanz.views = window.expanz.views || {};
 
-    window.expanz.views.RadioButtonsView = expanz.views.DataControlView.extend({
-        publishData: function () {
-            /* clean elements */
-            this.$el.html();
-            var that = this;
-            /* no external component needed just have to draw the checkboxes and handle the clicks */
+//    window.expanz.views.RadioButtonsView = expanz.views.DataControlView.extend({
+//        publishData: function () {
+//            /* clean elements */
+//            this.$el.html();
+//            var that = this;
+//            /* no external component needed just have to draw the checkboxes and handle the clicks */
 
-            _.each(this.model.get('xml').find('Row'), function (row) {
-                var rowId = $(row).attr('id');
-                var selected = boolValue($(row).attr('selected')) === true ? ' checked="checked" ' : '';
-                _.each($(row).find('Cell'), function (cell) {
-                    var text = $(cell).text();
-                    var id = that.model.id.replace(/\./g, "_") + "_" + rowId;
-                    that.$el.append("<div><input " + selected + " id='" + id + "' value='" + rowId + "' name='radio' type='radio'></input><span>" + text + "</span></div>");
+//            _.each(this.model.get('xml').find('Row'), function (row) {
+//                var rowId = $(row).attr('id');
+//                var selected = boolValue($(row).attr('selected')) === true ? ' checked="checked" ' : '';
+//                _.each($(row).find('Cell'), function (cell) {
+//                    var text = $(cell).text();
+//                    var id = that.model.id.replace(/\./g, "_") + "_" + rowId;
+//                    that.$el.append("<div><input " + selected + " id='" + id + "' value='" + rowId + "' name='radio' type='radio'></input><span>" + text + "</span></div>");
 
-                    /* handle radio button click */
-                    that.$el.find("#" + id).click(function () {
-                        /* send the delta to the server */
-                        that.model.updateItemSelected($(this).val());
-                    });
+//                    /* handle radio button click */
+//                    that.$el.find("#" + id).click(function () {
+//                        /* send the delta to the server */
+//                        that.model.updateItemSelected($(this).val());
+//                    });
 
-                });
-            });
+//                });
+//            });
 
-        }
+//        }
 
-    });
-});
+//    });
+//});
